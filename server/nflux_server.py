e03aec7465f333b8dab79c3a0681512279783b1b
bgnipp <bgnipp@gmail.com>
2017-09-10 17:15:48 -0700

    add server error logging

--- server/nflux_server.py
+++ server/nflux_server.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 from app import create_app, socketio
+import logging
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=False, port=5000)
+	logging.basicConfig(filename='error.log',level=logging.DEBUG)
+	socketio.run(app, host='0.0.0.0', debug=False, port=5000)
